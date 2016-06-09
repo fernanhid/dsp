e83d13088909233a4a985831a585322f0e3c51d3
fernanhid <fernanhid@gmail.com>
2016-06-09 13:33:55 -0400

    Finished Date Code

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,31 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+from datetime import datetime
+
+date_start_dt = datetime.strptime(date_start, "%m-%d-%Y")
+data_stop_dt = datetime.strptime(date_stop, "%m-%d-%Y")
+
+x = date_start_dt - data_stop_dt
+'Days Between: %r' %abs(x.days)
+
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+date_start_dt = datetime.strptime(date_start, "%m%d%Y")
+data_stop_dt = datetime.strptime(date_stop, "%m%d%Y")
+
+x = date_start_dt - data_stop_dt
+'Days Between: %r' %abs(x.days)
+
 ####c)  
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'
+
+date_start_dt = datetime.strptime(date_start, "%d-%b-%Y")
+data_stop_dt = datetime.strptime(date_stop, "%d-%b-%Y")
+
+x = date_start_dt - data_stop_dt
+'Days Between: %r' %abs(x.days)
