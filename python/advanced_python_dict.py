2de38e055e0286257757e98fc6a74695c5a17f55
fernanhid <fernanhid@gmail.com>
2016-06-11 22:55:29 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,3 +1,28 @@
+#Q6
+import pandas as pd
+data = pd.read_csv('faculty.csv', skipinitialspace = True)
+data['last_name'] = data.name.apply(lambda x: x.split()[-1])
+last_names = set(data.last_name)
+
+q6_data = data[['degree', 'title', 'email', 'last_name']]
+
+name_dict = {}
+for name in last_names:
+    x = data[data.last_name == name]
+    for index, row in x.iterrows():
+        name_dict.setdefault(row['last_name'], []).append(row[['degree', 'title', 'email']].tolist())
+                                                   
+sorted(name_dict.items())[:3]
+
+#Q7
+import pandas as pd
+data = pd.read_csv('faculty.csv', skipinitialspace = True)
+data['name_format'] = data.name.apply(lambda x: (x.split()[0], x.split()[-1]))
+
+q7_dict = data[['degree', 'title', 'email', 'name_format']].set_index('name_format').T.to_dict('list')
+sorted(q7_dict.items())[:3]
+
+#Q8
 import pandas as pd
 data = pd.read_csv('faculty.csv', skipinitialspace = True)
 data['last_name_format'] = data.name.apply(lambda x: (x.split()[-1], x.split()[0]))
