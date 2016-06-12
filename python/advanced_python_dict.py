859798676a4dc8e312399b515742345510365d9c
fernanhid <fernanhid@gmail.com>
2016-06-11 22:48:42 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,6 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+data = pd.read_csv('faculty.csv', skipinitialspace = True)
+data['last_name_format'] = data.name.apply(lambda x: (x.split()[-1], x.split()[0]))
+
+q8_dict = data[['degree', 'title', 'email', 'last_name_format']].set_index('last_name_format').T.to_dict('list')
+q8_dict.items()[:3]
