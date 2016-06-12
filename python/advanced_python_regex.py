a76e853c4643cb3151085492ed979f222c8988bd
fernanhid <fernanhid@gmail.com>
2016-06-11 23:43:27 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,22 @@
-PLACE YOUR CODE HERE
+#1. 
+
+from collections import Counter
+
+data = pd.read_csv('faculty.csv', skipinitialspace = True)
+list_ = [i.split() for i in data.degree]
+degrees = [o.replace('.', '') for i in list_ for o in i]
+
+Counter(degrees).items()
+
+#2. 
+data = pd.read_csv('faculty.csv', skipinitialspace = True)
+data['title'] = data.title.apply(lambda x: x.replace('is', 'of'))
+data.title.value_counts()
+
+#3. 
+[i for i in data.email]
+
+#4. 
+data = pd.read_csv('faculty.csv', skipinitialspace = True)
+data['mail_types'] = [i.split('@')[1] for i in data.email]
+data.mail_types.value_counts()
